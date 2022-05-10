259be212a4d2d09beba1330c4f5c532c5de06027
Akshay Jain <akshayjain@akshays-mbp.lan>
2022-05-10 11:11:05 -0700

    Fist push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component{
+  state = {
+    profileImg: 'https://images.creativetemplate.net/wp-content/uploads/2018/02/Blank-Menu-Design-Template.png'
+  }
+  imageHandler = (e) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if(reader.readyState === 2){
+        this.setState({profileImg: reader.result })
+        
+      }
+    }
+    reader.readAsDataURL(e.target.files[0])
+  }
+
+
+  render(){
+    const {profileImg} = this.state
+    return (
+      <div className="page">
+        <div className="container">
+          <h1 className="heading"> One Food Stop Application </h1>
+            <h2 className="heading" > Upload your food picture </h2>
+          <div className="img-holder" >
+            <img src={profileImg} alt ="" id="img" className="img"/>
+          </div>
+          <input type = "file" name = "image-upload" id = "input" accept = "image/*" onChange={this.imageHandler}/>
+          <div className="label" >
+            <label htmlFor="input" className="image-upload">
+              <i className="material-icons"> add_photo_alternate </i>
+              Choose your food photo
+            </label>
+          </div>
+        </div> 
+      </div>
+      
+    )
+  }
 }
-
-export default App;
+export default App
